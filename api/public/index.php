09ae9d736b5409e4498f19abe6a539c083ebc623
Vincent Chalamon <vincentchalamon@gmail.com>
2019-01-31 16:30:03 +0100

    Fix CS

--- api/public/index.php
+++ api/public/index.php
@@ -9,7 +9,7 @@ require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
+    if (!\class_exists(Dotenv::class)) {
         throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
     }
     (new Dotenv())->load(__DIR__.'/../.env');
@@ -19,7 +19,7 @@ $env = $_SERVER['APP_ENV'] ?? 'dev';
 $debug = $_SERVER['APP_DEBUG'] ?? ('prod' !== $env);
 
 if ($debug) {
-    umask(0000);
+    \umask(0000);
 
     Debug::enable();
 }
@@ -30,11 +30,11 @@ if ($_SERVER['HTTP_CF_VISITOR'] ?? false) {
 }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL);
+    Request::setTrustedProxies(\explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts(explode(',', $trustedHosts));
+    Request::setTrustedHosts(\explode(',', $trustedHosts));
 }
 
 $kernel = new Kernel($env, $debug);
