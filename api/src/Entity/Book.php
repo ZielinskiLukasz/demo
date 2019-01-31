8a164d0ed0ae1d6799b4126f2fbd6e4daed788b9
Vincent Chalamon <vincentchalamon@gmail.com>
2019-01-31 17:06:58 +0100

    Fix phpstan

--- api/src/Entity/Book.php
+++ api/src/Entity/Book.php
@@ -95,7 +95,7 @@ class Book
     public $publicationDate;
 
     /**
-     * @var Review[] The book's reviews
+     * @var Collection|ArrayCollection|Review[] The book's reviews
      *
      * @ORM\OneToMany(targetEntity=Review::class, mappedBy="book", orphanRemoval=true, cascade={"persist", "remove"})
      * @Groups("book:read")
